71477aeb79f7ac43525c02c307b34594d189986b
Tiziano Ghisotti <50081915+Tizianogh@users.noreply.github.com>
2021-12-13 18:30:16 +0100

    refactor(address): merge ressources (#7)

--- src/main/java/com/roadtripmaker/api/AddressRessource.java
+++ src/main/java/com/roadtripmaker/api/AddressRessource.java
@@ -3,12 +3,10 @@ package com.roadtripmaker.api;
 import com.roadtripmaker.domain.model.Address;
 import com.roadtripmaker.domain.model.Response;
 import com.roadtripmaker.service.AddressService;
+import com.roadtripmaker.service.GeodecodingImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.io.IOException;
@@ -16,12 +14,26 @@ import java.util.Map;
 
 import static java.time.LocalDateTime.now;
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController @RequestMapping("/address") @RequiredArgsConstructor public class AddressRessource {
     private final AddressService addressService;
+    private final GeodecodingImpl geodecoding;
 
     @PostMapping("/save") public ResponseEntity<Response> saveAddress(@RequestBody @Valid Address address) throws IOException {
         return ResponseEntity.ok(Response.builder().timeStamp(now()).data(Map.of("result", addressService.saveAddress(address)))
                 .message("adresse créée").status(CREATED).statusCode(CREATED.value()).build());
+    }
+
+    @GetMapping("/coordinates")
+    public ResponseEntity<Response> getCoordinates(@RequestBody @Valid Address address) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("results", geodecoding.computeGeoLocation(address)))
+                        .message("Position récupérées")
+                        .status(OK)
+                        .statusCode(OK.value()).build());
+
     }
 }

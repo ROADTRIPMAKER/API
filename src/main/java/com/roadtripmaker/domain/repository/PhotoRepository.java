3bb2978cb547304f0fa29edbb78a22ad9c39f9b8
Tiziano <tiziano.ghisotti@gmail.com>
2021-12-28 01:46:58 +0100

    refactor(photo): some adjustements

--- src/main/java/com/roadtripmaker/domain/repository/PhotoRepository.java
+++ src/main/java/com/roadtripmaker/domain/repository/PhotoRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface PhotoRepository extends JpaRepository<Photo, UUID> {
-    Photo findByUuid(UUID uuid);
+    Photo findByName(String name);
 }
